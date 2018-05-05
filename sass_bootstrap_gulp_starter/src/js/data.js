60228692ef507b15d6079b4ea2c451aca4fa125f
hmdevelop <mustafatopal2@gmail.com>
2018-05-05 09:00:03 +0300

    haberler eklendi dinamik

--- sass_bootstrap_gulp_starter/src/js/data.js
+++ sass_bootstrap_gulp_starter/src/js/data.js
@@ -1,11 +1,10 @@
 var haberler = ["haber1","haber2","haber3","haber4","haber5"];
 var news = [
 
-    {baslik:"baslık1" , body:"body1"},
-    {baslik:"baslık2" , body:"body2"},
-    {baslik:"baslık3" , body:"body3"},
-    {baslik:"baslık4" , body:"body4"},
-    {baslik:"baslık5" , body:"body5"},
+    {baslik:"Avrupa’nın karbon emisyonu 2017’de yüzde 1.8 arttı" , body:"Avrupa Birliği’nde fosil yakıtlardan kaynaklanan karbondioksit emisyonu 2017 yılında bir yıl öncesine göre yüzde 1.8 arttı.    Avrupa Birliği İstatistik Ofisi (Eurostat) tarafından yapılan açıklamada karbondioksit emisyonlarının, küresel ısınmanın başlıca sebebi olduğu ve AB’nin oluşturduğu sera gazı emisyonlarının yüzde 80’ini oluşturduğu vurgulandı.   Yapılan açıklamaya göre Finlandiya ve Danimarka karbondioksit emisyonları en büyük yıllık düşüşü kaydeden ülkeler olurken, Malta ve Estonya ise karbondioksit emisyonu en fazla yükselen ülkeler oldu."},
+    {baslik:"Finlandiya Parlamentosu Dışişleri Komisyonu Heyeti TBMM'de" , body:"TBMM Dışişleri Komisyonu Başkanı Volkan Bozkır, Finlandiya Parlamentosu Dışişleri Komisyonu Başkanı Matti Vanhanen ve beraberindeki heyet ile bir araya geldi."},
+    {baslik:"Futbol: UEFA 19 Yaş Altı Avrupa Şampiyonası" , body:"Finlandiya'da 16-29 Temmuz tarihleri arasında gerçekleştirilecek finallerin kura çekimi de 30 Mayıs Çarşamba günü ev sahibi ülkenin Vaasa şehrinde gerçekleştirilecek."},
+    {baslik:"Flakka nedir? Zombiye çeviren uyuşturucu Flakka hakkında bilinmeyenler" , body:"Flakka bağımlılarının videoları sosyal medyada büyük yankı uyandırmıştı. El yapımı olan bu uyuşturucu kokaine benzer bir etki gösterir. Fakat 2012 yılında Amerika’da yasaklanan sentetik uyuşturucu olan “banyo tuzları” gibi flakka da kokainden çok daha tehlikeli olma potansiyeline sahiptir. Florida’daki Nova Southeastern Üniversitesi’nde epidemiyolojist olan Jim Hall, flakkanın dozunu kontrol etmenin zor olduğunu ve ne kadar fazla tüketilirse o kadar ölümcül olabileceğini belirtmişti. Bu açıklama flakkanın ne kadar tehlikeli bir madde olduğunun anlaşılmasını sağlıyor."},
 
 
 
