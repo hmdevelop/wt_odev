e7c6b61f26695be12cede2a9eb02a3eee01d44b3
hmdevelop <mustafatopal2@gmail.com>
2018-05-05 17:27:47 +0300

    ..........

--- sass_bootstrap_gulp_starter/src/js/controllers/haberler.js
+++ sass_bootstrap_gulp_starter/src/js/controllers/haberler.js
@@ -1,5 +1,17 @@
-mainApp.controller('HaberlerController', function($scope) {
+mainApp.controller('HaberlerController', function($scope,$rootScope) {
     $scope.message = "Haberler mesaji";
     $scope.news = news;
+
+    $scope.cikis =   function (){
+ 
+
+           
+        $rootScope.login =  false ;
+       
+        console.log("çikildi");
+
+            window.location.href = 'http://localhost:3000/#/Haberler';
+    }
+
    
 });
