f217be20dd7c5d94ebffc6f36d39f0e4816980b4
Unknown <mustafa.topal6@ogr.sakarya.edu.tr>
2018-05-06 18:47:09 +0300

    delete ok

--- sass_bootstrap_gulp_starter/src/js/controllers/haberler.js
+++ sass_bootstrap_gulp_starter/src/js/controllers/haberler.js
@@ -3,6 +3,53 @@ mainApp.controller('HaberlerController', function($scope,$rootScope) {
     $scope.news = news;
 
 
+  //
+
+
+
+$scope.habersil =   function ($index){
+
+    $scope.selectedIndex = $index;
+
+
+        console.log($scope.selectedIndex);
+        $scope.news = news;
+        var t_news = $scope.news;
+        t_news.splice($scope.selectedIndex, 1);
+
+
+
+
+       
+           window.location.href = 'http://localhost:3000/#/Haberler';
+    
+
+   
+}
+
+
+
+//
+
+
+$scope.haberduzenle =   function (){
+
+    if( $rootScope.login == false){
+        window.alert("Lütfen Giriş Yapın");
+    }else{
+        var t_new = {baslik: $scope.t_baslik , body: $scope.t_body};
+
+        $scope.news = news;
+       var t_news = $scope.news;
+               t_news.push(t_new);
+           window.location.href = 'http://localhost:3000/#/Haberler';
+    }
+
+   
+}
+
+///
+
 
    
 });
